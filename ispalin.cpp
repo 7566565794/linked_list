fe8048a6b1234a293144ac54825eeebdec29379c
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-13 16:01:55 +0530

    Create ispalin.cpp

---
+++ ispalin.cpp
@@ -0,0 +1,28 @@
+// this program is to find whether a linklist is palindrome or not
+2
+3
+1 2 1
+4
+1 2 3 4//testcase
+bool isPalindrome(Node *head)
+{
+    //Your code here
+    if(head==NULL){
+        return true;
+    }
+    Node *p=head;
+    stack <int> s;
+    while(p!=NULL){
+        s.push(p->data);
+        p=p->next;
+    }
+    p=head;
+    while(s.empty()==false && p!=NULL){
+        if(s.top()!=p->data){
+            return false;
+        }
+        s.pop();
+        p=p->next;
+    }
+    return true;
+}
